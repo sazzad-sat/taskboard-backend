c66114cfea5efe11fac56f21aa1813215304bbc7
Sazzad Ahmed <sazzadahmed226@gmail.com>
2023-07-30 11:53:01 +0600

    fix attempt

--- routes/registerUser.ts
+++ routes/registerUser.ts
@@ -1,13 +1,15 @@
+import { Status } from "https://deno.land/std@0.193.0/http/http_status.ts";
 import { Router } from "https://deno.land/x/oak@v12.6.0/mod.ts";
 
 const router = new Router();
 
-router.post('/register-user', async ({request, response}) => {
-    console.log(await request.body().value)
-    
+router.post('/register-user', async ({ request, response }) => {
+    console.log(await request.body().value);
+
+    response.status = Status.OK;
     response.body = {
         success: true
-    }
-})
+    };
+});
 
 export default router;
